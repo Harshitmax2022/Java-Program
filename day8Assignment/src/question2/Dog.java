5399ecbe8ce6da151d419f857c0e6e9b8d0aca0f
Harshitmax2022 <harshittripathimax@gmail.com>
2022-05-28 14:24:28 +0530

    day8 assignment

---
+++ day8Assignment/src/question2/Dog.java
@@ -0,0 +1,12 @@
+package question2;
+
+public class Dog extends Animal {
+	Dog(String color) {  
+		super(color);  
+		// TODO Auto-generated constructor stub  
+		}  
+	@Override
+		void eat(String name) {  
+		System.out.println(name + " is eating ....");  
+		}  
+}
---
+++ day8Assignment/src/question3/Main.java
@@ -0,0 +1,14 @@
+package question3;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Dynamic polymorphism is a process or mechanism"
+				+ " in which a call to an overridden method is to resolve at runtime rather than "
+				+ "compile-time."
+				+ " It is also known as runtime polymorphism or dynamic method dispatch."
+				+ " We can achieve dynamic polymorphism by using the method overriding.");
+	}
+}
